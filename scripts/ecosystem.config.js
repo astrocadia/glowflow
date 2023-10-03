abfec68bafc835cf0f0ff72901db3219f7571482
astrocadia <pflueger.mike@gmail.com>
2023-10-03 16:21:59 -0400

    pm2 scripts added

---
+++ scripts/ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    apps : [
+        {
+            name: 'Glowflow Frontend Service',
+            cwd: '../src/glowflow-frontend-service',
+            script: 'npm run dev',
+            instances: 1,
+            watch: false,
+            env: {
+                PORT: 5173
+            },
+        },
+        {
+            name: 'Glowflow WS Service',
+            cwd: '../src/glowflow-ws-service',
+            script: 'node ws.js',
+            instances: 1,
+            watch: true,
+            ignore_watch: ['node_modules'],
+            env: {
+                PORT: 8080
+            },
+        },
+    ]
+}
